8d90951d6e3dd5454037a45776be504c95aa4771
rmanuel34 <49960223+rmanuel34@users.noreply.github.com>
2019-06-05 10:39:10 -0600

    Add files via upload

---
+++ Mi_comida_favorita.py
@@ -0,0 +1,11 @@
+comidas["pizza", "hamburguesa", "costilla agridulce", "hot dog", "pollo asado", "arroz chino", "papas fitas", "ensalada de papas", "pastelitos de maseca", "pastelitos de harina", "carne ala plancha", "carna asada", "bistec", "camarones empanizados", "lasaña", "sopa marinera", "ceviche", "pollo frito", "baleadas", "aros de cebolla"] 
+def imprimir_menu()
+	print("escojas su platillo favorito: ")
+	for comida in comidas
+		print(comidas)
+
+mi comida favoritas["pizza", "costilla", "hamburguesa", "pollo", "arroz chino"]
+def imprimir menu()
+	print("escoges 5 comidas favoritas": ")
+	for comida in comidas
+		print(comidas)
